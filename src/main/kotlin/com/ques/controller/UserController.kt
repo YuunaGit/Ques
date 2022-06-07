42849467ae1599f6e59c22b387e58013f8596d50
Yuuna <comaAberration@gmail.com>
2022-06-07 15:51:47 +0800

    Update UserController.kt

--- src/main/kotlin/com/ques/controller/UserController.kt
+++ src/main/kotlin/com/ques/controller/UserController.kt
@@ -1,7 +1,9 @@
 package com.ques.controller
 
 import com.ques.enum.Info
+import com.ques.hasNotBlank
 import com.ques.service.LoginServ
+import com.ques.service.ProfileServ
 import com.ques.service.RegisterServ
 import com.ques.utils.Res
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class UserController(
     val registerServ: RegisterServ,
-    val loginServ: LoginServ
+    val loginServ: LoginServ,
+    val profileServ: ProfileServ
 ) {
 
     @PostMapping("/register")
@@ -18,31 +21,46 @@ class UserController(
         name: String,
         email: String,
         password: String
-    ): Res = registerServ.register(name, email, password)
-
+    ): Res = if (name.hasNotBlank()
+        and email.hasNotBlank()
+        and password.hasNotBlank())
+        registerServ.register(name, email, password)
+    else Res(Info.ILLEGAL_PARAM)
 
     @PostMapping("/login")
-    fun loginByEmail(
+    fun login(
         email: String,
         password: String
-    ): Res = loginServ.login(email, password)
+    ): Res = if (email.hasNotBlank()
+        and password.hasNotBlank())
+        loginServ.login(email, password)
+    else Res(Info.ILLEGAL_PARAM)
 
     @PostMapping("/update/user/name")
     fun updateUserName(
-        name: String,
-        password: String
-    ): Res = Res(Info.SUCCESS)
+        password: String,
+        name: String
+    ): Res = if (password.hasNotBlank()
+        and name.hasNotBlank())
+        profileServ.updateUserName(password, name)
+    else Res(Info.ILLEGAL_PARAM)
 
     @PostMapping("/update/user/email")
     fun updateUserEmail(
-        name: String,
-        password: String
-    ): Res = Res(Info.SUCCESS)
+        password: String,
+        name: String
+    ): Res = if (password.hasNotBlank()
+        and name.hasNotBlank())
+        profileServ.updateUserName(password, name)
+    else Res(Info.ILLEGAL_PARAM)
 
-    @PostMapping("/update/user/password")
+    @PostMapping("/update/user/pwd")
     fun updateUserPwd(
-        name: String,
-        password: String
-    ): Res = Res(Info.SUCCESS)
+        old_password: String,
+        new_password: String
+    ): Res = if (old_password.hasNotBlank()
+        and new_password.hasNotBlank())
+        profileServ.updateUserName(old_password, new_password)
+    else Res(Info.ILLEGAL_PARAM)
 
 }
