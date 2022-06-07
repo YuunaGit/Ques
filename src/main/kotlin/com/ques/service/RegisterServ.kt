0c5517fa3ee97db1383522885dfdb4b10b054041
Yuuna <comaAberration@gmail.com>
2022-06-07 15:52:01 +0800

    Update RegisterServ.kt

--- src/main/kotlin/com/ques/service/RegisterServ.kt
+++ src/main/kotlin/com/ques/service/RegisterServ.kt
@@ -3,16 +3,15 @@ package com.ques.service
 import com.ques.entity.User
 import com.ques.enum.Info
 import com.ques.repository.UserRepo
-import com.ques.utils.Encoder
+import com.ques.utils.PwdUtil
 import com.ques.utils.Res
 import org.springframework.stereotype.Service
 
 @Service
 class RegisterServ(
     val userRepo: UserRepo,
-    val encoder: Encoder
+    val pwdUtil: PwdUtil
 ) {
-
     fun register(
         name: String,
         email: String,
@@ -24,16 +23,13 @@ class RegisterServ(
         val user = User(
             name = name,
             email = email,
-            password = encoder.encode(password)
+            password = pwdUtil.encode(password)
         )
         userRepo.save(user)
         return Res(Info.SUCCESS)
     }
 
-   fun emailUsed(
+    fun emailUsed(
        email: String
-   ): Boolean = userRepo.getUserByEmail(email).isNotEmpty()
-
-
-
+    ): Boolean = userRepo.getUserByEmail(email).isNotEmpty()
 }
