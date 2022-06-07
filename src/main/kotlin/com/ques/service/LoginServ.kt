339acd5ed26a81985961d7785839b6e6a0b2e066
Yuuna <comaAberration@gmail.com>
2022-06-07 15:51:57 +0800

    Update LoginServ.kt

--- src/main/kotlin/com/ques/service/LoginServ.kt
+++ src/main/kotlin/com/ques/service/LoginServ.kt
@@ -2,16 +2,15 @@ package com.ques.service
 
 import com.ques.enum.Info
 import com.ques.repository.UserRepo
-import com.ques.utils.Encoder
+import com.ques.utils.PwdUtil
 import com.ques.utils.Res
 import org.springframework.stereotype.Service
 
 @Service
 class LoginServ(
     val userRepo: UserRepo,
-    val encoder: Encoder
+    val pwdUtil: PwdUtil
 ) {
-
     fun login(
         email: String,
         password: String
@@ -23,7 +22,7 @@ class LoginServ(
 
         val user = users.first()
 
-        if(encoder.notMatch(password, user.password))
+        if(pwdUtil.notMatch(password, user.password))
             return Res(Info.PASSWORD_WRONG)
 
         return Res(Info.SUCCESS, user)
